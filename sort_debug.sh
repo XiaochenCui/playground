3eeab9ef70d17ed83dd3868270c4cac1891ec1f3
XiaochenCui <jcnlcxc.new@gmail.com>
2018-09-05 19:26:39 +0800

    Remove sort_debug

--- sort_debug.sh
+++
@@ -1,8 +0,0 @@
-printf "\n\tBytes' sum before sent to channel:\n\n"
-grep -o -E "block: \d+" fileops/debug.log | awk '{print $2}'
-printf "\n\tBytes' sum after receive from channel:\n\n"
-grep -o -E "encode: \d+" fileops/debug.log | awk '{print $2}'
-printf "\n\tArrays' sum before sort:\n\n"
-grep -o -E "before sort: \d+" fileops/debug.log | awk '{print $3}'
-printf "\n\tArrays' sum after sort:\n\n"
-grep -o -E "after sort: \d+" fileops/debug.log | awk '{print $3}'
