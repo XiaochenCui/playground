95b194a7951a4fa824621b86fe8e8bcf5a4d244d
XiaochenCui <jcnlcxc.new@gmail.com>
2024-12-14 11:16:34 -0800

    update

--- cpp/compiler/default_args.py
+++ cpp/compiler/default_args.py
@@ -1,38 +1,42 @@
 #!/usr/bin/env python3
+import difflib
+from typing import List
 import xiaochen_py
 
 
 def compare():
     gcc = "g++-13"
+    args_c = get_args(gcc, "c")
+    args_cpp = get_args(gcc, "c++")
+
+    diff = difflib.unified_diff(args_c, args_cpp, lineterm="")
+    for line in diff:
+        print(line)
+
     clang = "clang++-20"
 
+
+def get_args(compiler: str, lang: str) -> List[str]:
     gcc_output, _ = xiaochen_py.run_command(
-        f"echo | {gcc} -xc++ -v -",
+        f"echo | {compiler} -x{lang} ++ -v -",
         capture_tty=True,
         raise_on_failure=False,
         stream_output=False,
     )
-    print("--- output start ---")
-    print(gcc_output)
-    print("--- output end ---")
-
     lines = gcc_output.decode("utf-8").split("\n")
-    arg_start = False
-    arg_line = ""
-    for i, line in enumerate(lines):
-        print(f"{i}: {line}")
-        if line.startswith("COLLECT_GCC_OPTIONS"):
-            arg_start = True
-            arg_line = line
-            continue
 
-        if arg_start:
-            if line.startswith(" "):
-                arg_line += line
-            else:
-                arg_start = False
-                break
+    arg_line = None
+    for i, line in enumerate(lines):
+        if line.startswith(" /usr/libexec/gcc/x86_64-linux-gnu/13/collect2"):
+            arg_line = line
+            break
     print(f">>> default args: {arg_line}")
+
+    args = arg_line.split(" ")
+    for i, arg in enumerate(args):
+        print(f"{i}: {arg}")
+    args = line.split(" ")
+    return args[2:]
 
 
 if __name__ == "__main__":
