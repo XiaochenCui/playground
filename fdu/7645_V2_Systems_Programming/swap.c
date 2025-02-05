4835e755dd479bd60d6b4006a336c85024311832
Xiaochen Cui <jcnlcxc.new@gmail.com>
2025-02-05 14:32:34 -0800

    stash changes

--- fdu/7645_V2_Systems_Programming/swap.c
+++ fdu/7645_V2_Systems_Programming/swap.c
@@ -1,24 +1,54 @@
 #include <stdio.h>
 
-void swap(int *first, int *second) {
-    int temp;
-    temp = *first;
-    *first = *second;
-    *second = temp;
+void swapIntegers(int *first, int *second) {
+  int temp;
+  temp = *first;
+  *first = *second;
+  *second = temp;
+}
+
+typedef struct {
+  char firstName[20];
+  char lastName[20];
+} Person;
+
+void swapPersons(Person *first, Person *second) {
+  Person temp;
+  temp = *first;
+  *first = *second;
+  *second = temp;
 }
 
 int main() {
-    int first, second, temp;
+  int first, second, temp;
 
-    first = 4;
-    second = 6;
+  first = 4;
+  second = 6;
 
-    printf("BEFORE swap: first = %d, second = %d\n", first, second);
-    temp = first;
-    first = second;
-    second = temp;
-    printf("AFTER swap: first = %d, second = %d\n", first, second);
+  printf("BEFORE swap: first = %d, second = %d\n", first, second);
+  temp = first;
+  first = second;
+  second = temp;
+  printf("AFTER swap: first = %d, second = %d\n", first, second);
 
-    swap(&first, &second);
-    printf("AFTER swap: first = %d, second = %d\n", first, second);
+  swapIntegers(&first, &second);
+  printf("AFTER swap: first = %d, second = %d\n", first, second);
+
+  Person firstPerson, secondPerson;
+
+  strcpy(firstPerson.firstName, "John");
+  strcpy(firstPerson.lastName, "Doe");
+
+  strcpy(secondPerson.firstName, "Jane");
+  strcpy(secondPerson.lastName, "Smith");
+
+  printf("BEFORE swap: firstPerson = %s %s, secondPerson = %s %s\n",
+         firstPerson.firstName, firstPerson.lastName, secondPerson.firstName,
+         secondPerson.lastName);
+
+  swapPersons(&firstPerson, &secondPerson);
+
+  printf("AFTER swap: firstPerson = %s %s, secondPerson = %s %s\n",
+         firstPerson.firstName, firstPerson.lastName, secondPerson.firstName,
+         secondPerson.lastName);
 }
