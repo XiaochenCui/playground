f48223c4e496b5b0eade4f3a87e173f9f0921271
Xiaochen Cui <jcnlcxc.new@gmail.com>
2025-02-18 17:26:46 -0800

    stash changes

--- fdu/7645_V2_Systems_Programming/practice_1b/modify.c
+++ fdu/7645_V2_Systems_Programming/practice_1b/modify.c
@@ -1,49 +1,47 @@
+#include <stdio.h>
 
-
-void modifyString(char *item[], int numItems, int (*visitor)(int))
-{
-    for (int i = 0; i < numItems; i++)
-    {
+void modifyString(char *item[], int numItems, int (*visitor)(int)) {
+    for (int i = 0; i < numItems; i++) {
         char *p = item[i];
-        while (*p != '\0')
-        {
+        while (*p != '\0') {
             *p = visitor(*p);
             p++;
         }
     }
 }
 
-int toupper(int c)
-{
-    if (c >= 'a' && c <= 'z')
-    {
+int toupper(int c) {
+    if (c >= 'a' && c <= 'z') {
         return c - 'a' + 'A';
     }
     return c;
 }
 
-int tolower(int c)
-{
-    if (c >= 'A' && c <= 'Z')
-    {
+int tolower(int c) {
+    if (c >= 'A' && c <= 'Z') {
         return c - 'A' + 'a';
     }
     return c;
 }
 
-int main()
-{
-    char *item[] = {"hello", "world", "goodbye", "cruel", "world"};
+int main() {
+char str1[] = "Hello";
+char str2[] = "Linux";
+char str3[] = "Programming";
+
+// Use modifiable character arrays
+char *item[] = {str1, str2, str3};
+    printf("sizeof(item) = %lu\n", sizeof(item));
+    printf("sizeof(item[0]) = %lu\n", sizeof(item[0]));
     int numItems = sizeof(item) / sizeof(item[0]);
+
     modifyString(item, numItems, toupper);
-    for (int i = 0; i < numItems; i++)
-    {
+    for (int i = 0; i < numItems; i++) {
         printf("%s\n", item[i]);
     }
 
     modifyString(item, numItems, tolower);
-    for (int i = 0; i < numItems; i++)
-    {
+    for (int i = 0; i < numItems; i++) {
         printf("%s\n", item[i]);
     }
 
